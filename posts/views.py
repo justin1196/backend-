1addb3635f9ae3daaa6eb17d0a2f71eb508f775d
justin1196 <justin1196.jiseok@gmail.com>
2021-04-05 23:23:43 +0900

    과제

---
+++ posts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+def index(request):
+    if request.method == "GET":
+        return JsonResponse({'req_method': 'get'}, status=200)
+    elif request.method == "POST":
+        return JsonResponse({'req_method': "post"}, status=200)
+    else:
+        return JsonResponse({"error": "request method not allowed"}, status=405)
